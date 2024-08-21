2f7cda8529b51539b6281490836dba92a444c0af
MOHIBUL131589 <mohibul131589@gmail.com>
2024-08-21 15:11:00 +0600

    created a biker zone landing page with daisy UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'bike-primary': '#E76F51',
+        'bike-primary-bg': 'rgba(231, 111, 81, 0.1)'
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
